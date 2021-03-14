c227d7dd81268682d3e45268c635481a1cc56e5d
Wenjing81 <xxnfdg@gmail.com>
2021-03-14 19:44:25 +0100

    finish the function of displaying the city information when looking for the name.

--- app/src/main/java/com/allweather/network/PlaceService.kt
+++ app/src/main/java/com/allweather/network/PlaceService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PlaceService {
-
+//update the information in English.
     @GET("v2/place?token=${AllWeatherApplication.TOKEN}&lang=en_US")
     //zh_CN
     suspend fun searchPlaces(@Query("query") query: String): Response<QueryPlaceResult>
